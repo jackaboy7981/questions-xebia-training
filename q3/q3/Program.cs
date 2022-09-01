3449b9b140315bdd3c0ab2f01affe81aea07d85f
Jacob Abraham <jacobabraham7981@gmail.com>
2022-09-01 14:39:04 +0530

    question3 completed

---
+++ q3/q3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            float c;
+            Console.Write("Enter 2 numbers : \nno1 : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("no2: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (b!=0)
+            {
+            c = a / b;
+            Console.WriteLine(a + "/" + b + "=" + c);
+            }
+            else
+            {
+                Console.WriteLine("Division not possible as divisor = 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
