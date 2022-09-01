aa7517ff27a6213ae0a100eebc1a62f38598c5b3
Jacob Abraham <jacobabraham7981@gmail.com>
2022-09-01 14:31:16 +0530

    q2 completed

---
+++ q2/q2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Enter 2 numbers : \nno1 : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("no2: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine(a+"+"+b+"="+c);
+            Console.ReadKey();
+        }
+    }
+}
