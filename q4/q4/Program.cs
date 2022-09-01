a9c2ecb2f22ff56391effb5ff75bfbd4d2e64ab9
Jacob Abraham <jacobabraham7981@gmail.com>
2022-09-01 14:44:37 +0530

    q5 compeleted

---
+++ q4/q4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = -1 + 4 * 6;
+            Console.WriteLine(a);
+            a = (35 + 5) % 7;
+            Console.WriteLine(a);
+            a = 14 + -4 * 6 / 11;
+            Console.WriteLine(a);
+            a = 2 + 15 / 6 * 1 - 7 % 2;
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
